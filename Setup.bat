8ba5719fbdc93018261c62c77929b4a0730f4fec
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-03-26 18:27:35 +0100

    inwip: fixing setup.bat with new generator scripts

--- Setup.bat
+++ Setup.bat
@@ -4,7 +4,7 @@ pushd %~dp0
 
 git submodule update --init ULIS/Tools/Submodules/ProjectDependencies/ProjectDependencies
 call ULIS/Tools/Scripts/Windows/SetupHooks.bat
-if not exist "VisualStudioProjectConfig.cmake" copy "ULIS\Tools\DefaultConfig.cmake" "VisualStudioProjectConfig.cmake"
-if not exist "MinGWMakefefilesConfig.cmake" copy "ULIS\Tools\DefaultConfig.cmake" "MinGWMakefefilesConfig.cmake"
+if not exist "VisualStudio_Solution_MSVC_Config.cmake" copy "ULIS\Tools\DefaultConfig.cmake" "VisualStudio_Solution_MSVC_Config.cmake"
+if not exist "MinGW_Makefile_GCC_Config.cmake" copy "ULIS\Tools\DefaultConfig.cmake" "MinGW_Makefile_GCC_Config.cmake"
 call ULIS/Tools/Scripts/Windows/ProjectDependencies.bat -d
 pause
