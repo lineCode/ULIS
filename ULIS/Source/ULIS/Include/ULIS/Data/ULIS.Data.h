6ce4eb4e832f3392531970c020eaf4cd26e328c3
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-07-07 15:16:26 +0200

    infeat: add ULIS.Data.h, post-decl includer for other modules

---
+++ ULIS/Source/ULIS/Include/ULIS/Data/ULIS.Data.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+*
+*   ULIS
+*__________________
+*
+* ULIS.Core.h
+* Clement Berthaud - Layl
+* Please refer to LICENSE.md
+*/
+
+#pragma once
+
+#include "ULIS/Data/ULIS.Data.Op.h"
