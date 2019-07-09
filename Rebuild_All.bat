7e98b3ba6925bf48dd60a76f0368dfdb2ab4e971
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-07-10 00:04:43 +0200

    infix: change order in rebuild all

--- Rebuild_All.bat
+++ Rebuild_All.bat
@@ -4,14 +4,6 @@ pushd %~dp0
 
 call Generate_All.bat
 cd ULIS
-cd Generated_MinGW_Makefile_GCC
-cd Debug
-cmake --build .
-cd ..
-cd Release
-cmake --build .
-cd ..
-cd ..
 cd Generated_VisualStudio_Solution_CLANG_LLVM
 cmake --build . --config Release
 cmake --build . --config Debug
@@ -19,5 +11,12 @@ cd ..
 cd Generated_VisualStudio_Solution_MSVC
 cmake --build . --config Release
 cmake --build . --config Debug
+cd ..
+cd Generated_MinGW_Makefile_GCC
+cd Release
+cmake --build .
+cd ..
+cd Debug
+cmake --build .
 cd ..
 cd ..
