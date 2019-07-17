99835ae34bb0dd51294bf20f0747a99924308bb7
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-07-17 15:01:02 +0200

    infeat: add cpp for VectorSIMD

---
+++ ULIS/Source/ULIS/Source/ULIS/Base/ULIS.Base.VectorSIMD.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+*
+*   ULIS
+*__________________
+*
+* ULIS.Base.VectorSIMD.cpp
+* Clement Berthaud - Layl
+* Please refer to LICENSE.md
+*/
+
+#include "ULIS/Base/ULIS.Base.VectorSIMD.h"
+
+namespace ULIS {
+} // namespace ULIS
