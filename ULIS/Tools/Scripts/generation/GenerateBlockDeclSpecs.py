53d6cb3ab34a422e0c97c6ac75e987a74049ebbd
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-08-26 16:18:36 +0200

    infix: global reworking of available types & generator script in order to generate fewer template paths

--- ULIS/Tools/Scripts/generation/GenerateBlockDeclSpecs.py
+++ ULIS/Tools/Scripts/generation/GenerateBlockDeclSpecs.py
@@ -1,37 +1,35 @@
-tp = [ "uint8", "uint16", "uint32", "float", "double" ]
+tp = [ "uint8", "uint16", "uint32", "float" ]
 cm = [ "G", "RGB", "HSL", "HSV", "CMYK", "YUV", "Lab", "XYZ" ]
 ea = [ "hasAlpha", "noAlpha" ]
 nm = [ "normalized", "typeLimits" ]
 
-variants =	{
-	( "G", "noAlpha"	) 	: [ "G" ],
-	( "G", "hasAlpha"	) 	: [ "GA", "AG" ],
-	( "RGB", "noAlpha" 	) 	: [ "RGB", "BGR" ],
-	( "RGB", "hasAlpha" ) 	: [ "ARGB", "ABGR", "RGBA", "BGRA" ],
-	( "HSL", "noAlpha" 	) 	: [ "HSL" ],
-	( "HSL", "hasAlpha" ) 	: [ "HSLA", "AHSL" ],
-	( "HSV", "noAlpha" 	) 	: [ "HSV" ],
-	( "HSV", "hasAlpha" ) 	: [ "HSVA", "AHSV" ],
-	( "CMYK", "noAlpha" ) 	: [ "CMYK", "KCMY" ],
-	( "CMYK", "hasAlpha") 	: [ "CMYKA", "KCMYA", "ACMYK", "AKCMY" ],
-	( "YUV", "noAlpha" 	) 	: [ "YUV" ],
-	( "YUV", "hasAlpha" ) 	: [ "YUVA", "AYUV" ],
-	( "Lab", "noAlpha" 	) 	: [ "Lab" ],
-	( "Lab", "hasAlpha" ) 	: [ "LabA", "ALab" ],
-	( "XYZ", "noAlpha" 	) 	: [ "XYZ" ],
-	( "XYZ", "hasAlpha" ) 	: [ "XYZA", "AXYZ" ],
+variants =    {
+    ( "G", "noAlpha"    )       : [ "G" ],
+    ( "G", "hasAlpha"   )       : [ "GA", "AG" ],
+    ( "RGB", "noAlpha"  )       : [ "RGB", "BGR" ],
+    ( "RGB", "hasAlpha" )       : [ "ARGB", "ABGR", "RGBA", "BGRA" ],
+    ( "HSL", "hasAlpha" )       : [ "HSLA" ],
+    ( "HSV", "hasAlpha" )       : [ "HSVA" ],
+    ( "CMYK", "hasAlpha")       : [ "CMYKA" ],
+    ( "YUV", "hasAlpha" )       : [ "YUVA" ],
+    ( "Lab", "hasAlpha" )       : [ "LabA" ],
+    ( "XYZ", "hasAlpha" )       : [ "XYZA" ],
 }
 
 def is_decimal( iType ):
-	return  iType == "float" or iType == "double"
+    return  iType == "float" or iType == "double"
 
 for icm in cm:
-	for itp in tp: 
-		for iea in ea:
-			for inm in nm:
-				if( not is_decimal( itp ) and inm == "normalized" ):
-					continue
-					
-				var = ( icm, iea )
-				for ivar in variants[var]:
-					print( "ULIS_DECLSPEC(" + itp + "," + icm + "," + iea + "," + ivar + "," + inm + ")" )
+    for itp in tp: 
+        for iea in ea:
+            for inm in nm:
+                if( not is_decimal( itp ) and inm == "normalized" ):
+                    continue
+                    
+                if( is_decimal( itp ) and inm == "typeLimits" ):
+                    continue
+                    
+                var = ( icm, iea )
+                if var in variants.keys():
+                    for ivar in variants[var]:
+                        print( "ULIS_DECLSPEC(" + itp + "," + icm + "," + iea + "," + ivar + "," + inm + ")" )
