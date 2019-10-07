f971c591ad0de45cc650bcab76a05ebb65a1bd2c
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-10-07 16:38:35 +0200

    infix: build.cs

--- ULIS/Tools/Scripts/Unreal/ULIS.Build.cs
+++ ULIS/Tools/Scripts/Unreal/ULIS.Build.cs
@@ -53,7 +53,45 @@ public class ULIS : ModuleRules
         }
         else if ( Target.Platform == UnrealTargetPlatform.Mac )
         {
-            // TODO
+            string PlatformDir                      = Target.Platform.ToString();
+            string IncludeBase                      = Path.Combine( ModuleDirectory, "Redist", "Include" )
+            string ULIS_IncludePath                 = Path.Combine( IncludeBase, "ULIS" );
+            string LittleCMS_IncludePath            = Path.Combine( IncludeBase, "Little-CMS" );
+            string glm_IncludePath                  = Path.Combine( IncludeBase, "glm" );
+            string coal_IncludePath                 = Path.Combine( IncludeBase, "coal" );
+            string boot_preprocessor_IncludePath    = Path.Combine( IncludeBase, "boost_preprocessor" );
+            PublicSystemIncludePaths.AddRange(
+                new string[] {
+                      ULIS_IncludePath
+                    , LittleCMS_IncludePath
+                    , glm_IncludePath
+                    , coal_IncludePath
+                    , boot_preprocessor_IncludePath
+                }
+            );
+
+            string LibBase          = Path.Combine( ModuleDirectory, "Redist", "Lib", "Mac", "Xcode", "x64" );
+            string LibBaseRelease   = Path.Combine( LibBase, "Release" );
+            string LibBaseDebug     = Path.Combine( LibBase, "Debug" );
+            PublicLibraryPaths.Add( LibBaseRelease );
+            PublicLibraryPaths.Add( LibBaseDebug );
+            
+            string ULIS_LibName         = "libULIS1.0";
+            string LittleCMS_LibName    = "liblcms2";
+
+            if (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT) {
+                    ULIS_LibName        += "d";
+                    LittleCMS_LibName   += "d";
+            }
+
+            PublicAdditionalLibraries.AddRange(
+                new string[] {
+                      ULIS_LibName
+                    , LittleCMS_LibName,
+                }
+            );
+
+            PublicDefinitions.Add("WITH_ULIS=1");
         }
         else // unsupported platform
         {
