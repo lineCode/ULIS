2d33f5f87be87a242d4ad6c3fb26160cbfa4aa66
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-10-07 15:48:35 +0200

    fix

--- ULIS/Source/ULIS/Include/ULIS/Interface/ULIS.Interface.FX.h
+++ ULIS/Source/ULIS/Include/ULIS/Interface/ULIS.Interface.FX.h
@@ -11,8 +11,6 @@
 #include "ULIS/ULIS.Config.h"
 #include "ULIS/Base/ULIS.Base.BaseTypes.h"
 #include "ULIS/Base/ULIS.Base.PerformanceOptions.h"
-#include "ULIS/Color/ULIS.Color.CColor.h"
-#include "ULIS/Maths/ULIS.Maths.Geometry.h"
 
 ULIS_CLASS_FORWARD_DECLARATION( IBlock )
 
