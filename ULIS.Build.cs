ff7d95d0692034e7e304e47a7f132dd5a1d7ddac
Clement-Praxinos <clement.berthaud@praxinos.coop>
2019-12-18 14:32:52 +0100

    fix: use full path to reference libraries in .cs file (windows) fix: remove PublicSystemLibraryPaths as we use full path now

--- ULIS.Build.cs
+++ ULIS.Build.cs
@@ -1,4 +1,6 @@
-// Copyright 2019 Clement Berthaud, All Rights Reserved.
+// Copyright © 2018-2019 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
+
 using System;
 using System.IO;
 using UnrealBuildTool;
@@ -8,7 +10,8 @@ public class ULIS : ModuleRules
     public ULIS( ReadOnlyTargetRules Target ) : base( Target )
     {
         Type = ModuleType.External;
-        if (Target.Platform == UnrealTargetPlatform.Win64 ||
+
+        if( Target.Platform == UnrealTargetPlatform.Win64 ||
             Target.Platform == UnrealTargetPlatform.Win32 )
         {
             // string PlatformDir                      = Target.Platform.ToString();
@@ -31,27 +34,31 @@ public class ULIS : ModuleRules
             string LibBase          = Path.Combine( ModuleDirectory, "Redist", "Lib", "Win", "MSVC", "x64" );
             string LibBaseRelease   = Path.Combine( LibBase, "Release" );
             string LibBaseDebug     = Path.Combine( LibBase, "Debug" );
-            PublicSystemLibraryPaths.Add( LibBaseRelease );
-            PublicSystemLibraryPaths.Add( LibBaseDebug );
 
+            string LibBaseFull          = LibBaseRelease;
             string ULIS_LibName         = "ULIS1.0";
             string LittleCMS_LibName    = "lcms2";
 
-            if (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT) {
-                    ULIS_LibName        += "d";
-                    LittleCMS_LibName   += "d";
+            if( Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT )
+            {
+                LibBaseFull         = LibBaseDebug;
+                ULIS_LibName        += "d";
+                LittleCMS_LibName   += "d";
             }
+            
+            string ULIS_LibNameExt = ULIS_LibName + ".lib";
+            string LittleCMS_LibNameExt = LittleCMS_LibName + ".lib";
 
             PublicAdditionalLibraries.AddRange(
                 new string[] {
-                      ULIS_LibName + ".lib"
-                    , LittleCMS_LibName + ".lib"
+                      Path.Combine( LibBaseFull, ULIS_LibNameExt )
+                    , Path.Combine( LibBaseFull, LittleCMS_LibNameExt )
                 }
             );
 
-            PublicDefinitions.Add("WITH_ULIS=1");
+            PublicDefinitions.Add( "WITH_ULIS=1" );
         }
-        else if ( Target.Platform == UnrealTargetPlatform.Mac )
+        else if( Target.Platform == UnrealTargetPlatform.Mac )
         {
             // string PlatformDir                      = Target.Platform.ToString();
             string IncludeBase                      = Path.Combine( ModuleDirectory, "Redist", "Include" );
@@ -73,32 +80,33 @@ public class ULIS : ModuleRules
             string LibBase          = Path.Combine( ModuleDirectory, "Redist", "Lib", "Mac", "Xcode", "x64" );
             string LibBaseRelease   = Path.Combine( LibBase, "Release" );
             string LibBaseDebug     = Path.Combine( LibBase, "Debug" );
-            PublicSystemLibraryPaths.Add( LibBaseRelease );
-            PublicSystemLibraryPaths.Add( LibBaseDebug );
 
-            string LibBaseFull = LibBaseRelease;
-
+            string LibBaseFull          = LibBaseRelease;
             string ULIS_LibName         = "libULIS1.0";
             string LittleCMS_LibName    = "liblcms2";
             
-            if (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT) {
-                    ULIS_LibName        += "d";
-                    LittleCMS_LibName   += "d";
-                    LibBaseFull = LibBaseDebug;
+            if( Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT ) 
+            {
+                LibBaseFull         = LibBaseDebug;
+                ULIS_LibName        += "d";
+                LittleCMS_LibName   += "d";
             }
+
+            string ULIS_LibNameExt = ULIS_LibName + ".a";
+            string LittleCMS_LibNameExt = LittleCMS_LibName + ".a";
 
             PublicAdditionalLibraries.AddRange(
                 new string[] {
-                    Path.Combine( LibBaseFull, ULIS_LibName ) + ".a",
-                    Path.Combine( LibBaseFull, LittleCMS_LibName ) + ".a"
+                      Path.Combine( LibBaseFull, ULIS_LibNameExt )
+                    , Path.Combine( LibBaseFull, LittleCMS_LibNameExt )
                 }
             );
 
-            PublicDefinitions.Add("WITH_ULIS=1");
+            PublicDefinitions.Add( "WITH_ULIS=1" );
         }
         else // unsupported platform
         {
-            PublicDefinitions.Add("WITH_ULIS=0");
+            PublicDefinitions.Add( "WITH_ULIS=0" );
         }
     }
 }
