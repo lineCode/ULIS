ce31b1c18326993f35516a6c07c510140d19ce4b
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-03-16 17:53:24 +0100

    fix: forgot slash in macOS buildpath

--- ULIS/Tools/Scripts/Mac/FullBuildUE4_MacXcodex64.sh
+++ ULIS/Tools/Scripts/Mac/FullBuildUE4_MacXcodex64.sh
@@ -46,7 +46,7 @@ cd ..
 cd ..
 
 cp -a ${ROOT}/ULIS/Tools/EmbeddedDependencies/Coal/coal/Source/coal/Include/. ${ROOT}/Redist/Include/coal
-cp -a ${ROOT}/ULIS/Tools/EmbeddedDependencies/glm/glm. ${ROOT}/Redist/Include/glm/glm
+cp -a ${ROOT}/ULIS/Tools/EmbeddedDependencies/glm/glm/. ${ROOT}/Redist/Include/glm/glm
 cp -a ${ROOT}/ULIS/Tools/EmbeddedDependencies/preprocessor/include/. ${ROOT}/Redist/Include/boost_preprocessor
 cp -a ${ROOT}/ULIS/Tools/EmbeddedDependencies/Little-CMS/include/. ${ROOT}/Redist/Include/Little-CMS
 cp -a ${ROOT}/ULIS/Source/ULIS/Include/. ${ROOT}/Redist/Include/ULIS
