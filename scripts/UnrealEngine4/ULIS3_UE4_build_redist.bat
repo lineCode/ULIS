66bd3638e80d050f13e71de186f6d170e7982580
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-05-26 16:29:04 +0200

    feat: make UE4 module scripts build ULIS3 as RelWithDebInfo instead

--- scripts/UnrealEngine4/ULIS3_UE4_build_redist.bat
+++ scripts/UnrealEngine4/ULIS3_UE4_build_redist.bat
@@ -6,13 +6,6 @@ pushd %~dp0
 :: Check if we're next to ULIS3 directory
 IF NOT EXIST ULIS3 EXIT /B 1
 
-:: Decide a generator
-SET VERSION=2019
-IF NOT "%1"=="" SET VERSION=%1
-SET GENERATOR="Visual Studio 16 2019"
-IF %VERSION%==2017 SET GENERATOR="Visual Studio 15 2017 Win64"
-ECHO %GENERATOR%
-
 :: Make dir for build if not exist
 IF NOT EXIST ULIS3_FullBuildUE4_WinMSVCx64_Redist ( MKDIR ULIS3_FullBuildUE4_WinMSVCx64_Redist )
 
@@ -31,15 +24,13 @@ IF EXIST cmake_install.cmake ( del cmake_install.cmake )
 IF EXIST CMakeCache.txt ( del CMakeCache.txt )
 
 :: Rebuild Project
-cmake   -G %GENERATOR%          ^
-        -A x64                  ^
-        -DULIS3_BUILD_SHARED=ON ^
+:: Replace with "Visual Studio 15 2017 Win64" if needed
+cmake   -G "Visual Studio 16 2019"                          ^
+        -A x64                                              ^
+        -DULIS3_BUILD_SHARED=ON                             ^
         ../../ULIS3
 
-
-cmake --build . --config Release
-:: Decide if embed debug
-IF "%2"=="-d" ( cmake --build . --config Debug )
+cmake --build . --config RelWithDebInfo
 
 :: go up to build dir
 cd ..
@@ -58,12 +49,9 @@ mkdir lib
 
 :: CD to root and copy
 cd ../../
-XCOPY /E /C /I /Q /H /K /Y /B ".\ULIS3\include"                                                         "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\include\ULIS3"
-XCOPY /E /C /I /Q /H /K /Y /B ".\ULIS3\3rdparty\glm"                                                    "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\include\glm"
-COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\Release\ULIS3.lib"   "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\lib\ULIS3.lib"
-COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\Release\ULIS3.dll"   "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\bin\ULIS3.dll"
-COPY /Y "ULIS3\scripts\UnrealEngine4\ULIS.Build.cs"                                                     "ULIS3_FullBuildUE4_WinMSVCx64_Redist\ULIS.Build.cs"
-
-IF "%2"=="-d" ( COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\Debug\ULIS3d.lib"    "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\lib\ULIS3d.lib" )
-IF "%2"=="-d" ( COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\Debug\ULIS3d.dll"    "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\bin\ULIS3d.dll" )
-IF "%2"=="-d" ( COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\Debug\ULIS3d.pdb"    "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\bin\ULIS3d.pdb" )
+XCOPY /E /C /I /Q /H /K /Y /B ".\ULIS3\include"                                                                 "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\include\ULIS3"
+XCOPY /E /C /I /Q /H /K /Y /B ".\ULIS3\3rdparty\glm"                                                            "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\include\glm"
+COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\RelWithDebInfo\ULIS3.lib"    "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\lib\ULIS3.lib"
+COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\RelWithDebInfo\ULIS3.dll"    "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\bin\ULIS3.dll"
+COPY /Y "ULIS3_FullBuildUE4_WinMSVCx64_Redist\Generated_VisualStudio_Solution_MSVC\RelWithDebInfo\ULIS3.pdb"    "ULIS3_FullBuildUE4_WinMSVCx64_Redist\redist\bin\ULIS3.pdb"
+COPY /Y "ULIS3\scripts\UnrealEngine4\ULIS.Build.cs"                                                             "ULIS3_FullBuildUE4_WinMSVCx64_Redist\ULIS.Build.cs"
