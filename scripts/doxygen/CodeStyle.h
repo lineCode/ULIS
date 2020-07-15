b7b45c979a44f4fe8645bb1da179e5f1723a8a12
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-07-15 12:22:06 +0200

    experimenting with api style

--- scripts/doxygen/CodeStyle.h
+++ scripts/doxygen/CodeStyle.h
@@ -232,3 +232,63 @@ int main() {
 
     return  0;
 }
+
+
+//////////////////////////////////////////////////////
+// Advanced Out Of Order Multithreaded Scheduling
+#include <ULIS3>
+
+int main() {
+    using namespace ::ul3;
+
+    FThreadPool pool( FThreadPool::MaxWorkers() - 1 );
+    FCommandQueue queue( thread_pool );
+    tFormat documentFormat = Format_RGBA8;
+    FContext ctx( documentFormat, command_queue, Ctx_PrefetchDispatch, SelectPerf_SSE );
+
+    FHostDeviceInfo info = FHostDeviceInfo::Detect();
+
+    FSchedulerPolicy cacheEfficientPolicy( Run_Multi, Schedule_Chunks, ItemSize_ChunkLength, info.CacheLineSize_L1 );
+    FSchedulerPolicy scanLinePolicy( Run_Multi, Schedule_Scanlines )
+    FSchedulerPolicy monoPolicy( Run_Mono );
+
+    FVec2ui16 size( 1024, 1024 );
+    FRasterImage2D drawing( documentFormat, size );
+    FRasterImage2D overlay( documentFormat, size );
+    FGPUTexture2D texture( documentFormat, size );
+
+    FTaskEvent evt_fill_drawing = ctx.Fill( drawing, FColor::FromHex( 0xF00 ), cacheEfficientPolicy );
+    FTaskEvent evt_fill_overlay = ctx.Fill( overlay, FColor::FromHex( 0xBA2 ), cacheEfficientPolicy, &evt_fill_drawing );
+    evt_fill_overlay.Poll();
+
+    FTaskEvent evt_blend = ctx.AlphaBlendAA( overlay, drawing, overlay.Rect(), FVec2f( 0, 0 ), Blend_Normal, 1.f, scanLinePolicy );
+    ctx.Fence();
+
+    ctx.UploadToGPU( drawing, texture, monoPolicy, &evt_blend );
+    ctx.Flush();
+
+    return  0;
+}
+
+
+//////////////////////////////////////////////////////
+// Easy In Order MonoThread Scheduling
+#include <ULIS3>
+
+int main() {
+    using namespace ::ul3;
+    FContext ctx;
+
+    FVec2i size( 1024, 1024 );
+    FRasterImage2D drawing( size );
+    FRasterImage2D overlay( size );
+    FGPUTexture2D texture( size );
+
+    ctx.Fill( drawing, FColor::FromHex( 0xF00 ) );
+    ctx.Fill( overlay, FColor::FromHex( 0xBA2 ) );
+    ctx.AlphaBlendAA( overlay, drawing, overlay.Rect(), FVec2f( 0, 0 ), Blend_Normal, 1.f );
+    ctx.UploadToGPU( drawing, texture );
+
+    return  0;
+}
+
