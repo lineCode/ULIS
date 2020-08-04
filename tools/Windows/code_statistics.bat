126e01cc1e14715e2e164a4835f515c16e28744c
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-08-04 11:43:25 +0200

    Update tools code_statistics
    Output corresponding file type and location for line count.

--- tools/Windows/code_statistics.bat
+++ tools/Windows/code_statistics.bat
@@ -3,10 +3,14 @@ setlocal
 pushd %~dp0
 
 cd ../../include
+echo "Num lines in include/*.h"
 powershell "dir -Recurse *.h | Get-Content | Measure-Object -Line"
+echo "Num lines in include/*.cpp"
 powershell "dir -Recurse *.cpp | Get-Content | Measure-Object -Line"
 cd ../source
+echo "Num lines in source/*.h"
 powershell "dir -Recurse *.h | Get-Content | Measure-Object -Line"
+echo "Num lines in source/*.cpp"
 powershell "dir -Recurse *.cpp | Get-Content | Measure-Object -Line"
 
 PAUSE
