afac685ccd3bc9a9839481751025a6b6f85b7a9c
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-08-10 21:50:19 +0200

    feat: add tools for ASCII images

---
+++ include/Image/ASCII.h
@@ -0,0 +1,21 @@
+// Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
+/*
+*
+*   ULIS3
+*__________________
+*
+* @file         String.h
+* @author       Clement Berthaud
+* @brief        This file provides the declaration for the FString class.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Core/Core.h"
+
+ULIS_NAMESPACE_BEGIN
+const char* gASCII_Greyscale_StandardRamp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+const char* gASCII_Greyscale_10LevelsRamp = "@%#*+=-:. "
+ULIS_NAMESPACE_END
+
