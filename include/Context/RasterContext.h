7a262e1ce02151571d109be78083dce47a5e009a
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-08-18 00:25:02 +0200

    wip: start imp RasterContext

--- include/Context/RasterContext.h
+++ include/Context/RasterContext.h
@@ -5,9 +5,9 @@
 *   ULIS
 *__________________
 *
-* @file         FRasterContext2D.h
+* @file         FRasterContext.h
 * @author       Clement Berthaud
-* @brief        This file provides the declaration for the FRasterContext2D class.
+* @brief        This file provides the declaration for the FRasterContext class.
 * @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
 * @license      Please refer to LICENSE.md
 */
@@ -16,10 +16,18 @@
 
 ULIS_NAMESPACE_BEGIN
 /////////////////////////////////////////////////////
-/// @class      FRasterContext2D
-/// @brief      The FRasterContext2D class provides a context for calling image
+// Invocations parameters
+class FBlendArguments;
+
+/////////////////////////////////////////////////////
+// Invocations function pointers
+typedef void ( *tBlendInvocation )( const FBlendArguments& );
+
+/////////////////////////////////////////////////////
+/// @class      FRasterContext
+/// @brief      The FRasterContext class provides a context for calling image
 ///             raster and composition functions for a given format.
-/// @details    The FRasterContext2D works on FRasterImage2D and is bound to a
+/// @details    The FRasterContext works on FRasterImage2D and is bound to a
 ///             specific format. It prefetches the dispatched implementation to
 ///             select the fastest method according to the format, and its
 ///             specializations if available, and according to the current runtime
@@ -29,14 +37,20 @@ ULIS_NAMESPACE_BEGIN
 ///             \sa FThreadPool
 ///             \sa FDevice
 ///             \sa FCommandQueue
-class ULIS_API FRasterContext2D
+class ULIS_API FRasterContext
 {
 public:
     /*! Destructor */
-    ~FRasterContext2D();
+    ~FRasterContext();
 
     /*! Constructor */
-    FRasterContext2D();
+    FRasterContext();
+
+private:
+    tBlendInvocation  fpBlend;
+    tBlendInvocation  fpBlendColor;
+    tBlendInvocation  fpBlendTiled;
+    tBlendInvocation  fpBlendAlpha;
 };
 
 ULIS_NAMESPACE_END
