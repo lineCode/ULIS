aa7eca4904cc4f92893409d713114c1595a18f00
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-09-02 10:34:24 +0200

    feat: added branding watermak bit marking

---
+++ source/Misc/Branding.cpp
@@ -0,0 +1,36 @@
+// Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
+/*
+*
+*   ULIS
+*__________________
+*
+* @file         Branding.cpp
+* @author       Clement Berthaud
+* @brief        This file provides the definition for the Branding entry point functions.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Misc/Branding.h"
+#include "Core/AXI.h"
+#include "Image/Block.h"
+#include "Math/Math.h"
+
+ULIS_NAMESPACE_BEGIN
+void ApplyBranding( FBlock& iBlock ) {
+    FString info = FullLibraryInformationString();
+    const char* str = info.Data();
+    uint64 len_str = info.Size();
+    uint64 len_bytes = iBlock.BytesTotal();
+    uint64 len = FMath::Min( len_str, len_bytes / 2 );
+    uint8* data = iBlock.Bits();
+    for( uint64 i = 0; i < len; i += 2 ) {
+        uint8 el = static_cast< uint8 >( str[ i / 2 ] );
+        data[i] = ( data[i] & 0xFC ) | ( el & 0x3 );
+        data[i+1] = ( data[i+1] & 0xFC ) | ( ( el & 0xC ) >> 2 );
+    }
+}
+
+ULIS_NAMESPACE_END
+
---
+++ source/Misc/Branding.h
@@ -0,0 +1,28 @@
+// Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
+/*
+*
+*   ULIS
+*__________________
+*
+* @file         Branding.h
+* @author       Clement Berthaud
+* @brief        This file provides the declaration for the Branding entry point functions.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Core/Core.h"
+
+ULIS_NAMESPACE_BEGIN
+/*!
+    Applies a bit pattern to parts of the image to brand it as being processed
+    within ULIS. The branding is akin to a watermark but is almost invisible.
+    It messes with the bits of the image to encode a pattern that will identify
+    the image without any doubt. This can be used to track illegal uses of the
+    ULIS library.
+*/
+void ApplyBranding( FBlock& iBlock );
+
+ULIS_NAMESPACE_END
+
